a6fe9766ec0bcbd1f31cfb4a66d43eb213607d71
Roman <roman@caspiantechnologies.org>
2018-06-28 11:44:07 +0700

    Implement subscribing to camera availability change events
    SPATIUM-299

--- src/windows/CameraInfoProxy.js
+++ src/windows/CameraInfoProxy.js
@@ -1,13 +1,67 @@
+let watcher = null;
+let subscribers = [];
+let subscriberIdCounter = 1;
+let cameraAvailable = null;
+
+async function isVideoCameraAvailable() {
+    const devices = await Windows.Devices.Enumeration.DeviceInformation.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture);
+    return devices.length > 0;
+}
+
+async function onUpdate() {
+    const availableNow = await isVideoCameraAvailable();
+    if (cameraAvailable !== availableNow) {
+        cameraAvailable = availableNow;
+        subscribers.forEach(subscriber => subscriber.callback(cameraAvailable));
+    }
+}
+
 cordova.commandProxy.add('CameraInfo', {
 
     isAvailable: async function (successCallback, errorCallback, params) {
         try {
-            const devices = await Windows.Devices.Enumeration.DeviceInformation.findAllAsync(Windows.Devices.Enumeration.DeviceClass.videoCapture);
-            const cameraAvailable = devices.length > 0;
-            successCallback(cameraAvailable);
+            successCallback(await isVideoCameraAvailable());
         }
         catch (error) {
-            errorCallback(e);
+            errorCallback(error);
         }
-    }
+    },
+
+    subscribeToAvailabilityChanges: async function (successCallback, errorCallback, availabilityChangeCallback) {
+        try {
+            const subscriberId = subscriberIdCounter++;
+            subscribers.push({
+                callback: availabilityChangeCallback,
+                id: subscriberId,
+            });
+            successCallback(subscriberId);
+
+            if (!watcher) {
+                watcher = Windows.Devices.Enumeration.DeviceInformation.createWatcher();
+                watcher.addEventListener('enumerationcompleted', onUpdate);
+                watcher.addEventListener('updated', onUpdate);
+                watcher.start();
+            } else {
+                availabilityChangeCallback(await isVideoCameraAvailable());
+            }
+        }
+        catch (error) {
+            errorCallback(error);
+        }
+    },
+
+    unsubscribeFromAvailabilityChanges: async function (successCallback, errorCallback, callbackId) {
+        try {
+            const subscriberIndex = subscribers.findIndex(subscriber => subscriber.id === callbackId);
+            if (subscriberIndex === -1) {
+                throw new Error(`Failed to find callback with id=${callbackId}`);
+            }
+            subscribers.splice(subscriberIndex, 1);
+            successCallback(callbackId);
+        }
+        catch (error) {
+            errorCallback(error);
+        }
+    },
+
 });
--- www/CameraInfo.js
+++ www/CameraInfo.js
@@ -15,3 +15,26 @@ exports.isAvailable = function () {
         }
     });
 };
+
+exports.subscribeToAvailabilityChanges = function (onChangeCallback) {
+    return new Promise(function (success, error) {
+        if (isWindows()) {
+            exec(success, error, 'CameraInfo', 'subscribeToAvailabilityChanges', onChangeCallback);
+        }
+        else {
+            success(0);
+            onChangeCallback(true);
+        }
+    });
+};
+
+exports.unsubscribeFromAvailabilityChanges = function (callbackId) {
+    return new Promise(function (success, error) {
+        if (isWindows()) {
+            exec(success, error, 'CameraInfo', 'unsubscribeFromAvailabilityChanges', callbackId);
+        }
+        else {
+            success(callbackId);
+        }
+    });
+};
